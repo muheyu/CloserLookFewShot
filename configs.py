3a0787d5942963ae6af15839a0f0095c03a81b17
Yang Lj <eejyang@qq.com>
2020-07-09 20:01:01 +0800

    2020.7.9

--- configs.py
+++ configs.py
@@ -1,6 +1,6 @@
 save_dir                    = './model'
 data_dir = {}
-data_dir['CUB']             = './filelists/CUB/' 
+data_dir['CUB']             = 'E:/Database/cub/' 
 data_dir['miniImagenet']    = 'E:/Database/miniImagenet/'
-data_dir['omniglot']        = './filelists/omniglot/' 
-data_dir['emnist']          = './filelists/emnist/' 
+data_dir['omniglot']        = 'E:/Database/omniglot/' 
+data_dir['emnist']          = 'E:/Database/emnist/' 
